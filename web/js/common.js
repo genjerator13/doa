7e717eb386932835cd40cbd90e919c356bc21bf5
spoonbou <spoonbou@gmail.com>
2014-04-09 16:24:04 +0200

    boat details

--- web/js/common.js
+++ web/js/common.js
@@ -1,2 +1,27 @@
+//gallery = function() {
+//    return{
+//        init: function() {
+//            $('.pictureThumbs img').hover(function() {
+//                $('img#picture').attr('src', this.src);
+//            });
+//        }
+//    };
+//}();
 
+            $('.pictureThumbs img').hover(function() {
+                $('img#picture').attr('src', this.src);
+            });
+            
 
+function initialize() {
+    var mapProp = {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+            
+//    $(document).ready(function() {
+//        alert('Hello, World!');
+//    });
