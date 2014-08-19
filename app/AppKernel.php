7b3c51fe48441a3981b00776e252690e814d9dcf
genjerator13 <root@genjerator.(none)>
2014-08-19 19:07:24 +0200

    fix

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),            
-            new Bc\Bundle\BootstrapBundle\BcBootstrapBundle(),
+            //new Bc\Bundle\BootstrapBundle\BcBootstrapBundle(),
             new APY\DataGridBundle\APYDataGridBundle(),
             new Genemu\Bundle\FormBundle\GenemuFormBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
