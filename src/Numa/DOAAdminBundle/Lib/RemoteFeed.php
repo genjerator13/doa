d9b5401648b338177c2b8fd4492688f1c03b0b46
genjerator13 <root@genjerator.(none)>
2014-11-08 12:55:11 +0100

    ftp upload

--- src/Numa/DOAAdminBundle/Lib/RemoteFeed.php
+++ src/Numa/DOAAdminBundle/Lib/RemoteFeed.php
@@ -50,7 +50,8 @@ class RemoteFeed extends ContainerAware {
     }
 
     public function fetchRemoteProperties() {
-        
+
+
         if (empty($this->properties)) {
             if (self::URL == $this->entity->getImportMethod() || self::UPLOAD == $this->entity->getImportMethod()) {
                 if (self::XML == $this->entity->getImportFormat()) {
@@ -62,14 +63,22 @@ class RemoteFeed extends ContainerAware {
                         }
                         break;
                     }
-                }elseif (self::CSV == $this->entity->getImportFormat()) {                                                            
+                } elseif (self::CSV == $this->entity->getImportFormat()) {
                     $handleSource = $this->entity->getAbsolutePath();
                     //echo strtolower(substr( $this->entity->getImportSource(), 0, 6 )).":::";
-                    if(strtolower(substr( $this->entity->getImportSource(), 0, 6 )) == "ftp://"){
-                        
+                    /*
+                    if (strtolower(substr($this->entity->getImportSource(), 0, 6)) == "ftp://") {
+
                         $handleSource = $this->entity->getImportSource();
+                        $upload_path = \Numa\DOAAdminBundle\NumaDOAAdminBundle::getContainer()->getParameter('upload_feed');
+                        $local_file .= $this->entity->getID() . "/ftp_source.csv";
+die($local_file);
+                        ftp_get($ftpExplode[0], $local_file, $ftpExplode[1], FTP_ASCII);
+                        
                     }
                     //die($handleSource);
+                     * 
+                     */
                     if (($handle = fopen($handleSource, "r")) !== FALSE) {
                         $row = fgetcsv($handle);
                         //set the properties from header
@@ -99,7 +108,7 @@ class RemoteFeed extends ContainerAware {
     }
 
     public function getRemoteItems() {
-
+        $rowCount = 0;
         if (self::URL == $this->entity->getImportMethod()) {
             if (self::XML == $this->entity->getImportFormat()) {
                 $xml_obj = simplexml_load_file($this->source);
@@ -112,12 +121,30 @@ class RemoteFeed extends ContainerAware {
                 return $this->xml2array($this->items);
             }
         } elseif (self::CSV == $this->entity->getImportFormat()) {
+            $filename = $this->entity->getAbsolutePath();
+            if (strtolower(substr($this->entity->getImportSource(), 0, 6)) == "ftp://") {
 
-            if (($handle = fopen($this->entity->getAbsolutePath(), "r")) !== FALSE) {
-                $rowCount = 0;
+                $handleSource = $this->entity->getImportSource();
+                $upload_path = \Numa\DOAAdminBundle\NumaDOAAdminBundle::getContainer()->getParameter('upload_feed');
+                if(!file_exists($upload_path .  $this->entity->getID())){
+                    mkdir($upload_path .  $this->entity->getID());
+                    echo $upload_path .  $this->entity->getID();
+                }
+                
+                $local_file = $upload_path .  $this->entity->getID() . "/ftp_source.csv";
+                $conn = self::getFtpConnection($handleSource);
+                $ftpExplode = explode("/", $handleSource);
+
+                ftp_get($conn, $local_file, $ftpExplode[count($ftpExplode)-1], FTP_ASCII);
+                
+                $filename = $local_file;
+            }
+            if (($handle = fopen($filename, "r")) !== FALSE) {
+                
+                
                 while (($row = fgetcsv($handle)) !== FALSE) {
                     //var_dump($row); // process the row.
-
+                    
                     if ($rowCount > 0) {
                         foreach ($header as $key => $value) {
                             $tmp[$value] = $row[$key];
@@ -127,10 +154,10 @@ class RemoteFeed extends ContainerAware {
                         $header = $row;
                     }
                     $rowCount++;
+                    
                 }
             }
         }
-        
         return $this->items;
     }
 
@@ -138,6 +165,33 @@ class RemoteFeed extends ContainerAware {
         foreach ($this->items as $item) {
             $item = new Item();
         }
+    }
+
+    static function getFtpConnection($uri) {
+        // Split FTP URI into:
+        // $match[0] = ftp://username:password@sld.domain.tld/path1/path2/
+        // $match[1] = ftp://
+        // $match[2] = username
+        // $match[3] = password
+        // $match[4] = sld.domain.tld
+        // $match[5] = /path1/path2/
+        preg_match("/ftp:\/\/(.*?):(.*?)@(.*?)\/(.*)/i", $uri, $match);
+
+        // Set up a connection
+        print_r($match);
+        $conn = ftp_connect($match[3]);
+
+        // Login
+        if (ftp_login($conn, $match[1], $match[2])) {
+            // Change the dir
+            //ftp_chdir($conn, $match[5]);
+            ftp_pasv($conn, true);
+            // Return the resource
+            return $conn;
+        }
+
+        // Or retun null
+        return null;
     }
 
     static function xml2array($xmlObject, $out = array()) {
@@ -160,21 +214,22 @@ class RemoteFeed extends ContainerAware {
                 break;
         }
     }
-/*
-    public function xml2array($xml) {
-        $arr = array();
 
-        foreach ($xml as $element) {
-            $tag = $element->getName();
-            $e = get_object_vars($element);
-            if (!empty($e)) {
-                $arr[$tag] = $element instanceof SimpleXMLElement ? xml2array($element) : $e;
-            } else {
-                $arr[$tag] = trim($element);
-            }
-        }
+    /*
+      public function xml2array($xml) {
+      $arr = array();
 
-        return $arr;
-    }
-*/
+      foreach ($xml as $element) {
+      $tag = $element->getName();
+      $e = get_object_vars($element);
+      if (!empty($e)) {
+      $arr[$tag] = $element instanceof SimpleXMLElement ? xml2array($element) : $e;
+      } else {
+      $arr[$tag] = trim($element);
+      }
+      }
+
+      return $arr;
+      }
+     */
 }
