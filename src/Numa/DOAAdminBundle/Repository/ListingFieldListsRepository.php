80cab4e55018ac9194b36e0a5c1d4ea53cde3f9e
genjerator13 <root@genjerator.(none)>
2015-02-16 13:25:58 +0100

    ' sign remove

--- src/Numa/DOAAdminBundle/Repository/ListingFieldListsRepository.php
+++ src/Numa/DOAAdminBundle/Repository/ListingFieldListsRepository.php
@@ -13,7 +13,7 @@ class ListingFieldListsRepository extends EntityRepository {
      * @return type
      */
     public function findOneByValue($propertyName, $listing_field_id) {
-
+        $propertyName = str_replace("'", "", $propertyName);
         $q = 'SELECT l FROM NumaDOAAdminBundle:ListingfieldLists l WHERE 
                     ( l.listing_field_id = ' . $listing_field_id . ' AND
                     (l.value like \'' . $propertyName . '\'  OR 
