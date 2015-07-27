4610bf4f2a8f8adc9d84ff8a82536d3902ddf684
genjerator13 <root@genjerator.(none)>
2015-07-28 01:28:00 +0200

    dsfgfg

--- devtoall.sh
+++ devtoall.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # My first script
 
-echo "UPDATING cjvr2.dealersonair.com"
-ROOT_PATH="/home/cjvr/public_html"
-SCRIPT="cd ${ROOT_PATH};git pull;php app/console cache:clear --env=prod;chmod -R 777 ${ROOT_PATH}/app/cache ${ROOT_PATH}/app/logs;php ${ROOT_PATH}/app/console assetic:dump"
-echo "${SCRIPT}"
-ssh root@doa.numadns.com "${SCRIPT}"
-
+git checkout develop;
+git checkout master;
+git merge develop;
+git checkout peak;
+git merge master;
+git checkout semoauto;
+git merge master;
+git checkout colorado;
+git merge master;
+git checkout wvow;
+git merge master;
+git checkout cjvr;
+git merge master;
+git push;
+git checkout develop;
