d4f08ab68d13543326c288772d2278e8cd628d3d
genjerator13 <root@genjerator.(none)>
2015-07-31 13:08:21 +0200

    update peak

--- update_peak.sh
+++ update_peak.sh
@@ -5,5 +5,5 @@ echo "UPDATING peakdealersonline.com"
 ROOT_PATH="/home/peakdealersonline/public_html"
 SCRIPT="cd ${ROOT_PATH};git pull;php app/console cache:clear --env=prod;chmod -R 777 ${ROOT_PATH}/app/cache ${ROOT_PATH}/app/logs;php ${ROOT_PATH}/app/console assetic:dump"
 echo "${SCRIPT}"
-ssh root@doa.numadns.com "${SCRIPT}"
+ssh root@198.169.134.134 "${SCRIPT}"
 
