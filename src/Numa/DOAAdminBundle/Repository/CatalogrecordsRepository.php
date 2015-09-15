ae60dbb0072f060cbaed70b4bd2455a1ab2fc017
genjerator13 <e.medjesi@gmail.com>
2015-09-15 16:04:12 +0200

    login by username and email

--- src/Numa/DOAAdminBundle/Repository/CatalogrecordsRepository.php
+++ src/Numa/DOAAdminBundle/Repository/CatalogrecordsRepository.php
@@ -2,10 +2,14 @@
 
 namespace Numa\DOAAdminBundle\Repository;
 
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Doctrine\ORM\EntityRepository;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
-class CatalogrecordsRepository extends EntityRepository {
+class CatalogrecordsRepository extends EntityRepository implements UserProviderInterface{
+
 
     public function xfindByDCategory($dcatId) {
         $qb = $this->getEntityManager()
@@ -47,6 +51,51 @@ class CatalogrecordsRepository extends EntityRepository {
         //$qb->getMaxResults(1);
         $query = $qb->getQuery();
         return $query->getResult();
+    }
+
+    public function loadUserByUsername($username)
+    {
+
+        $user = $this->findOneByUsernameOrEmail($username);
+
+        if (!$user) {
+            throw new UsernameNotFoundException('No user found for username '.$username);
+        }
+
+        return $user;
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        $class = get_class($user);
+        if (!$this->supportsClass($class)) {
+            throw new UnsupportedUserException(sprintf(
+                'Instances of "%s" are not supported.',
+                $class
+            ));
+        }
+
+        if (!$refreshedUser = $this->find($user->getId())) {
+            throw new UsernameNotFoundException(sprintf('User with id %s not found', json_encode($user->getId())));
+        }
+
+        return $refreshedUser;
+    }
+
+    public function supportsClass($class)
+    {
+        return $this->getEntityName() === $class
+        || is_subclass_of($class, $this->getEntityName());
+    }
+
+    public function findOneByUsernameOrEmail($username)
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.username = :username OR u.email = :email')
+            ->setParameter('username', $username)
+            ->setParameter('email', $username)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 
 
