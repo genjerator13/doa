8818f8ef5439ec357a98242dfe6cda1d9e403e35
genjerator13 <e.medjesi@gmail.com>
2015-09-29 08:23:27 +0200

    git fix

--- src/Numa/DOASiteBundle/Controller/SandboxController.php
+++ src/Numa/DOASiteBundle/Controller/SandboxController.php
@@ -11,7 +11,6 @@ class SandboxController extends Controller {
 
     public function indexAction() {
         $data = array('key' => '16ab1a10-2daf-0130-dd76-005056be005f', 'password' => 'arkansas22');
-
         $url = "http://www.machinefinder.com/dealer_families/6926/machine_feed.xml";
         $handle = curl_init($url);
         curl_setopt($handle, CURLOPT_POST, true);
