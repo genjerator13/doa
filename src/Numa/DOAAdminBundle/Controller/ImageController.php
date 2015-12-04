5731e903862810bb7d528221596f3a7dd89d1bf3
genjerator13 <e.medjesi@gmail.com>
2015-12-04 17:21:45 +0100

    image sortable

--- src/Numa/DOAAdminBundle/Controller/ImageController.php
+++ src/Numa/DOAAdminBundle/Controller/ImageController.php
@@ -22,24 +22,25 @@ use Doctrine\Common\Collections\Criteria;
  * Item controller.
  *
  */
-class ImageController extends Controller {
+class ImageController extends Controller
+{
 
-    public function showAction(Request $request, $id) {
+    public function showAction(Request $request, $id)
+    {
         $em = $this->getDoctrine()->getManager();
 
         $item = $em->getRepository('NumaDOAAdminBundle:Item')->find($id);
         $ImageList = $em->getRepository('NumaDOAAdminBundle:Listingfield')->findOneBy(array('caption' => 'Image List'));
         //\Doctrine\Common\Util\Debug::dump($ImageList->getId());
         $criteria = Criteria::create()
-                ->where(Criteria::expr()->eq("fieldName", "Image List"))
-                ->orderBy(array('sortOrder' => Criteria::ASC))
-        ;
+            ->where(Criteria::expr()->eq("fieldName", "Image List"))
+            ->orderBy(array('sortOrder' => Criteria::ASC));
         $images = $item->getItemField()->matching($criteria);
         ///
 
         foreach ($images as $image) {
 
-            if(substr( $image->getFieldStringValue(), 0, 4 ) !== "http") {
+            if (substr($image->getFieldStringValue(), 0, 4) !== "http") {
                 $imagemanagerResponse = $this->container
                     ->get('liip_imagine.controller')
                     ->filterAction(
@@ -65,63 +66,16 @@ class ImageController extends Controller {
             throw $this->createNotFoundException('Unable to find Item entity.');
         }
 
-//        $uploadForm = $this->createAddImagesForm($item->getId());
-//        $uploadForm->handleRequest($request);
-//
-//
-//        if ($uploadForm->isValid()) {
-//            // If form is valid
-//            // Get file
-//            $files = $request->files->get('form');
-//
-//            foreach ($files as $file) {
-//
-//                if ($file instanceof \Symfony\Component\HttpFoundation\File\UploadedFile &&
-//                        ($file->getMimeType() == 'image/jpeg' ||
-//                        $file->getMimeType() == 'image/png' ||
-//                        $file->getMimeType() == 'image/gif')) {
-//
-//                    $upload_url = $this->container->getParameter('upload_url');
-//                    $upload_path = $this->container->getParameter('upload_path');
-//                    $itemField = new ItemField();
-//
-//                    $itemField->handleImage($file, $upload_path, $upload_url, $item->getImportFeed(), 0, true, $item->getId().'_'.time());
-//                    $item->setDateUpdated(new \DateTime());
-//                    $itemField->setItem($item);
-//                    $itemField->setListingfield($ImageList);
-//                    $em->persist($itemField);
-//                }
-//                $em->flush();
-//            }
-//            return $this->redirect($this->generateUrl('item_images', array('id' => $id)));
-//        }
-
 
         return $this->render('NumaDOAAdminBundle:Item:images.html.twig', array(
-                    'item' => $item,
-                    'images' => $images,
-                    //'addimages' => $uploadForm->createView(),
+            'item' => $item,
+            'images' => $images,
+            //'addimages' => $uploadForm->createView(),
         ));
     }
 
-//    public function createAddImagesForm($item_id) {
-//        $data = array();
-//        return $this->createFormBuilder($data)
-//                        //->setAction($this->generateUrl('images_add', array('item_id' => $item_id)))
-//                        ->add('Picture1', 'file', array('label' => 'Picture 1', 'required' => false))
-//                        ->add('Picture2', 'file', array('label' => 'Picture 2', 'required' => false))
-//                        ->add('Picture3', 'file', array('label' => 'Picture 3', 'required' => false))
-//                        ->add('Picture4', 'file', array('label' => 'Picture 4', 'required' => false))
-//                        ->add('Picture5', 'file', array('label' => 'Picture 5', 'required' => false))
-//                        ->add('Upload', 'submit', array('label' => 'Upload'))
-//                        ->getForm();
-//    }
-
-//    public function orderPlus() {
-//
-//    }
-
-    public function uploadAction(Request $request,$id){
+    public function uploadAction(Request $request, $id)
+    {
 
         $em = $this->getDoctrine()->getManager();
         $form = $this->createFormBuilder()->getForm();
@@ -133,14 +87,15 @@ class ImageController extends Controller {
         if ($file instanceof \Symfony\Component\HttpFoundation\File\UploadedFile &&
             ($file->getMimeType() == 'image/jpeg' ||
                 $file->getMimeType() == 'image/png' ||
-                $file->getMimeType() == 'image/gif')) {
+                $file->getMimeType() == 'image/gif')
+        ) {
             $item = $em->getRepository('NumaDOAAdminBundle:Item')->find($id);
             $ImageList = $em->getRepository('NumaDOAAdminBundle:Listingfield')->findOneBy(array('caption' => 'Image List'));
             $upload_url = $this->container->getParameter('upload_url');
             $upload_path = $this->container->getParameter('upload_path');
             $itemField = new ItemField();
 
-            $itemField->handleImage($file, $upload_path, $upload_url, $item->getImportFeed(), 0, true, $item->getId().'_'.time());
+            $itemField->handleImage($file, $upload_path, $upload_url, $item->getImportFeed(), 0, true, $item->getId() . '_' . time());
             $item->setDateUpdated(new \DateTime());
             $itemField->setItem($item);
             $itemField->setListingfield($ImageList);
@@ -148,6 +103,24 @@ class ImageController extends Controller {
             $em->flush();
         }
 
+        die();
+    }
+
+    public function setImageOrderAction(Request $request)
+    {
+        $orders = $request->request->get('orders');
+        $ordersArray = json_decode($orders, true);
+        $em = $this->getDoctrine()->getManager();
+        foreach ($ordersArray as $key => $order) {
+            $id   = (intval($key));
+            $order = (intval($order));
+            $qb = $em->getRepository("NumaDOAAdminBundle:ItemField")->createQueryBuilder('if')
+                ->update()
+                ->set('if.sort_order', $order)
+                ->where('if.id=' . $id);
+            dump($qb);
+            $qb->getQuery()->execute();
+        }
         die();
     }
 
