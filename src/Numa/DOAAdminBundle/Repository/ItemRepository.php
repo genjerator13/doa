55d5366c4dcec698646ef9ed2cf43b095547c511
genjerator13 <e.medjesi@gmail.com>
2016-04-07 17:15:35 +0200

    dashboard changes

--- src/Numa/DOAAdminBundle/Repository/ItemRepository.php
+++ src/Numa/DOAAdminBundle/Repository/ItemRepository.php
@@ -3,6 +3,7 @@
 namespace Numa\DOAAdminBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Numa\DOAAdminBundle\Entity\Catalogrecords;
 use Numa\DOAAdminBundle\Entity\Importfeed;
 use Numa\DOAAdminBundle\Entity\Item;
 use Numa\DOAAdminBundle\Entity\ItemField;
@@ -542,6 +543,34 @@ class ItemRepository extends EntityRepository
         $query = $qb->getQuery();
         $res = $query->getResult(); //->getResult();
         return $res;
+    }
+
+    public function countAllListings($active=1,$sold=0,$dealer=false){
+        $suffix = "";
+        if($dealer instanceof Catalogrecords){
+            $suffix = " and i.dealer_id=".$dealer->getId();
+        }
+        $sql = "select count(*) as count from item i WHERE i.active=$active and i.sold=$sold".$suffix;
+dump($dealer);
+        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
+        $res = $stmt->execute();
+        $res = $stmt->fetch();
+        return $res['count'];
+
+    }
+
+    public function countAllViews($active=1,$sold=0,$dealer=false){
+        $suffix = "";
+        if($dealer instanceof Catalogrecords){
+            $suffix = " and i.dealer_id=".$dealer->getId();
+        }
+        $sql = "select sum(i.views) as count from item i WHERE i.active=$active and i.sold=$sold".$suffix;
+
+        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
+        $res = $stmt->execute();
+        $res = $stmt->fetch();
+        return $res['count'];
+
     }
 
 
--- src/Numa/DOADMSBundle/Controller/DefaultController.php
+++ src/Numa/DOADMSBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace Numa\DOADMSBundle\Controller;
 
+use Numa\DOAAdminBundle\Entity\Catalogrecords;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -9,23 +10,36 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('NumaDOADMSBundle:Default:index.html.twig');
+        $em = $this->get('doctrine.orm.entity_manager');
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $dealer=false;
+
+        if ($user instanceof Catalogrecords) {
+            $dealer = $user;
+
+        }
+        $totalListings = $em->getRepository('NumaDOAAdminBundle:Item')->countAllListings(1,0,$dealer);
+        $totalViews = $em->getRepository('NumaDOAAdminBundle:Item')->countAllViews(1,0,$dealer);
+
+        return $this->render('NumaDOADMSBundle:Default:index.html.twig',
+            array('totalListings' => $totalListings, 'totalViews' => $totalViews));
     }
 
-    public function dealerChooserAction(Request $request){
-        $dealers=array();
+    public function dealerChooserAction(Request $request)
+    {
+        $dealers = array();
         $dealer_id = $request->get('dealer');
-        if(!empty($dealer_id)){
+        if (!empty($dealer_id)) {
             $dealer = $this->get('doctrine.orm.entity_manager')->getRepository('NumaDOAAdminBundle:Catalogrecords')->find($dealer_id);
             $session = $this->get('session');
-            $session->set('dealer',$dealer);
+            $session->set('dealer', $dealer);
 
             return $this->redirectToRoute('dms_home');
 
         }
-        if($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')){
+        if ($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
             $dealers = $this->get('doctrine.orm.entity_manager')->getRepository('NumaDOAAdminBundle:Catalogrecords')->findAll();
-            return $this->render('NumaDOADMSBundle::dealerChooser.html.twig',array('dealers'=>$dealers));
+            return $this->render('NumaDOADMSBundle::dealerChooser.html.twig', array('dealers' => $dealers));
         }
         return $this->render('NumaDOADMSBundle::dealerChooser.html.twig');
     }
