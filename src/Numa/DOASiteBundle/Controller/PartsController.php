48566cfa49fff22d90147618b4846d2b895343c6
root <vladimir25233@hotmail.com>
2016-05-05 18:36:57 +0200

    Parts message

--- src/Numa/DOASiteBundle/Controller/PartsController.php
+++ src/Numa/DOASiteBundle/Controller/PartsController.php
@@ -6,30 +6,74 @@ use Numa\DOADMSBundle\Entity\PartRequest;
 use Numa\DOADMSBundle\Form\PartRequestType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityRepository;
-use Numa\Util\Util as Util;
-use Symfony\Component\Stopwatch\Stopwatch;
+
 
 class PartsController extends Controller {
 
-    public function partAction() {
+    public function partAction(Request $request) {
 
-        $form= $this->CreateCreateForm();//TODO
-        return $this->render('NumaDOASiteBundle:Parts:parts_form.html.twig', array('form' => $form->createView())
-        );
+        $entity = new PartRequest();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+            $request->getSession()
+                ->getFlashBag()
+                ->add('success', 'Success!');
+
+            return $this->redirect($this->generateUrl('part_form'));
+
+        }
+
+        return $this->render('NumaDOASiteBundle:Parts:parts_form.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
     }
 
-    public function CreateCreateForm() {
-        //$action = $this->generateUrl('part_request');
 
-        $form = $this->createForm(new PartRequestType(), new PartRequest(), array(
-            //'action' => $action,
+
+    public function createAction(Request $request)
+    {
+        $entity = new PartRequest();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+            return $this->redirect($this->generateUrl('partrequest'));
+
+        }
+
+        return $this->render('NumaDOADMSBundle:PartRequest:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a PartRequest entity.
+     *
+     * @param PartRequest $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(PartRequest $entity)
+    {
+        $form = $this->createForm(new PartRequestType(), $entity, array(
+            'action' => $this->generateUrl('part_form'),
             'method' => 'POST',
         ));
 
-        $form->add('submit', 'submit', array('label' => 'Request','attr'=>array('class'=>"btn btn-primary")));
+        $form->add('submit', 'submit', array('label' => 'Create'));
 
         return $form;
     }
+
 }
 
