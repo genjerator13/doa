39f3efadcb055fe4b05496dc0176786058db97be
genjerator13 <e.medjesi@gmail.com>
2016-05-31 12:32:09 +0200

    details boat dialogs

--- src/Numa/DOAApiBundle/Controller/ListingFormRESTController.php
+++ src/Numa/DOAApiBundle/Controller/ListingFormRESTController.php
@@ -10,6 +10,9 @@ namespace Numa\DOAApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ListingFormRESTController extends Controller
 {
@@ -30,6 +33,46 @@ class ListingFormRESTController extends Controller
     {
         $listingForm = $this->getDoctrine()->getRepository('NumaDOADMSBundle:ListingForm')->findBy(array('dealer_id'=>$dealer_id));
         return $listingForm;
+    }
+
+    public function postListingFormAction(Request $request)
+    {
+        $post = $request->getContent();
+        $postD = json_decode($post);
+        $data =$request->request->get('numa_doadmsbundle_listingform');
+        //get customer by email ($data['email']
+        //if the customer is not found create new one based by data
+        //create new listingform
+        //set type test drive
+        
+        $em = $this->getDoctrine()->getManager();
+        //check if already inserted
+        $response = new JsonResponse(
+            array(
+                'message' => 'Success',
+                'action' => '',
+                400));
+
+
+
+        return $response;
+/*
+ * array:7 [
+  "cust_name" => ""
+  "cust_last_name" => ""
+  "contact_by" => "0"
+  "email" => ""
+  "phone" => ""
+  "date_drive" => array:3 [
+    "month" => "1"
+    "day" => "1"
+    "year" => "2011"
+  ]
+  "_token" => "BwQm4mcdOjWbZvSf9FHt9XXdzJyZsOqUzyRAI40PGeI"
+]
+
+ */
+
     }
 
 }
