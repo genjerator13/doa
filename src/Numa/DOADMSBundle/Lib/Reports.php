50837ca094bfc54632211dfaaa64c7719d0c2f8d
dzvlado <vladimir25233@hotmail.com>
2016-07-20 17:44:21 +0200

    sales reports added

--- src/Numa/DOADMSBundle/Lib/Reports.php
+++ src/Numa/DOADMSBundle/Lib/Reports.php
@@ -97,4 +97,81 @@ class Reports
         return $response;
 
     }
+
+    public $inventorySalesFields = array(
+        "B"=>array("customer","Customer Name"),
+        "C"=>array("date_billing","Date"),
+        "D"=>array("invoice_nr","Invoice #"),
+        "E"=>array("tid_year","Year"),
+        "F"=>array("tid_make","Make"),
+        "G"=>array("tid_model","Model"),
+        "H"=>array("item:stock_nr","Stock #"),
+        "I"=>array("tid_vin","Vin #"),
+        "J"=>array("sale_price","Sale Price"),
+        "K"=>array("less_trade_in","Less Trade In"),
+        "L"=>array("lien_on_trade_in","Lien On Trade In"),
+        "M"=>array("total_due","Total Due"),
+    );
+    public function billingReportSalesXls($entities)
+    {
+
+        $rendererName = \PHPExcel_Settings::PDF_RENDERER_MPDF;
+        $rendererLibrary = 'mPDF';
+        $rendererLibraryPath = (dirname(__FILE__) . '/../../../../vendor/mpdf/mpdf'); //works
+
+        if (!\PHPExcel_Settings::setPdfRenderer(
+            $rendererName, $rendererLibraryPath
+        )) {
+            die(
+                'NOTICE: Please set the $rendererName and $rendererLibraryPath values' .
+                '<br />' .
+                'at the top of this script as appropriate for your directory structure'
+            );
+        }
+        //disable profiler
+        if ($this->container->has('profiler')) {
+            $this->container->get('profiler')->disable();
+        }
+        $em = $this->container->get("doctrine.orm.entity_manager");
+
+        $download_path = $this->container->getParameter('upload_dealer');
+        $phpExcelObject = $this->container->get('phpexcel')->createPHPExcelObject();
+        //dump($phpExcelObject);
+        $phpExcelObject->getProperties()->setCreator("DOA")
+            ->setLastModifiedBy("DOA")
+            ->setTitle("DOA sales report")
+            ->setSubject("DOA sales report")
+            ->setDescription("DOA sales report")
+        ;
+        $phpExcelObject->setActiveSheetIndex(0);
+
+        foreach($this->inventorySalesFields as $key=>$field){
+            $phpExcelObject->getActiveSheet()->setCellValue($key . "2", $field[1]);
+        }
+
+        foreach($this->inventorySalesFields as $key=>$field){
+            $i=3;
+            foreach($entities as $entity) {
+                //dump($field);
+                $phpExcelObject->getActiveSheet()->setCellValue($key . $i, $entity->get($field[0]));
+                $i++;
+            }
+        }
+        // die();
+        //dump($entities);die();
+        // adding headers
+        // create the writer
+        $writer = $this->container->get('phpexcel')->createWriter($phpExcelObject, 'Excel5');
+        // create the response
+        $response = $this->container->get('phpexcel')->createStreamedResponse($writer);
+        $response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
+        $response->headers->set('Content-Disposition', 'attachment;filename=billing_report.xls');
+        $response->headers->set('Pragma', 'public');
+        $response->headers->set('Cache-Control', 'maxage=1');
+        $writer = $this->container->get('phpexcel')->createWriter($phpExcelObject, 'Excel5');
+        $writer->save($download_path . 'CustomerDetailsReport_' . "test" . '.xls');
+
+        return $response;
+
+    }
 }
