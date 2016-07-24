3f7fdd6f9bb6721c2777cf11e806d5101a366c4c
root <e.medjesi@gmail.com>
2016-07-24 14:38:21 +0200

    finance form fixes

--- src/Numa/DOASiteBundle/Controller/FinanceController.php
+++ src/Numa/DOASiteBundle/Controller/FinanceController.php
@@ -21,16 +21,33 @@ class FinanceController extends Controller implements DealerSiteControllerInterf
 
     }
 
+
     public function newAction(Request $request)
     {
-        // create a task and give it some dummy data for this example
-        $finance = new Finance();
 
+        $finance = new Finance();
+        $loanTerms = array(""=>'Chose A Term',
+            "6 Months"=>"6 Months",
+            "12 Months"=>"12 Months",
+            "18 Months"=>"18 Months",
+            "24 Months"=>"24 Months",
+            "30 Months"=>"30 Months",
+            "36 Months"=>"36 Months",
+            "42 Months"=>"42 Months",
+            "48 Months"=>"48 Months",
+            "54 Months"=>"54 Months",
+            "60 Months"=>"60 Months",
+            "72 Months"=>"72 Months",
+            "84 Months"=>"84 Months",
+            "96 Months"=>"96 Months",
+            "108 Months"=>"108 Months",
+            "120 Months"=>"120 Months",
+        );
         $form = $this->createFormBuilder($finance)
 
-            ->add('applicant_type','choice',array('choices'=>array('Individual','')))
+            ->add('applicant_type','choice',array('choices'=>array('Individual','Joint')))
             ->add('amount_required', null)
-            ->add('loan_term','choice',array('label'=>'* Loan Term', 'required' => true, 'choices'=>array('Chose A Team','')))
+            ->add('loan_term','choice',array('label'=>'* Loan Term', 'required' => true, 'choices'=>$loanTerms))
             ->add('down_payment', null, array('label'=> '* Down Payment', 'required' => true))
             ->add('trade_in','choice',array('label'=>'* Trade-In', 'required' => true, 'choices'=>array('Yes','No')))
 
@@ -49,24 +66,24 @@ class FinanceController extends Controller implements DealerSiteControllerInterf
             ->add('cell_phone', null, array('label'=>'Cell Phone'))
             ->add('address', null, array('label'=>'* Address', 'required' => true))
             ->add('city', null, array('label'=>'* City', 'required' => true))
-            ->add('state','choice',array('label'=>'* State', 'required' => true,'choices'=>array('AR','')))
+            ->add('state','text',array('label'=>'* State/Prov', 'required' => true))
             ->add('zip_code', null, array('label'=>'* ZIP Code', 'required' => true))
 
             ->add('ssn_sin_nr', null, array('label'=>'* SSN / SIN. No.', 'required' => true))
-            ->add('birth_date', 'date', array('label'=>'* Date of Birth', 'required' => true))
-            ->add('residence_type','choice',array('label'=>'* Residence Type', 'required' => true,'choices'=>array('Own','')))
+            ->add('birth_date', 'date', array('label'=>'* Date of Birth', 'required' => true,'years'       => range(date('Y')-16, date('Y') - 100, -1)))
+            ->add('residence_type','choice',array('label'=>'* Residence Type', 'required' => true,'choices'=>array('Own'=>"Own",'Rent'=>"Rent")))
             ->add('monthly_payment', null, array('label'=> '* Monthly Payment', 'required' => true))
-            ->add('at_residence', 'date', array('label'=> '* At Residence', 'required' => true))
+            ->add('at_residence', 'date', array('label'=> '* At Residence', 'required' => true,'years'       => range(date('Y'), date('Y') - 50, -1)))
 
 
             ->add('employer', null, array('label'=> '* Employer', 'required' => true))
             ->add('occupation', null, array('label'=> '* Occupation', 'required' => true))
             ->add('monthly_income', null, array('label'=> '* Monthly Income', 'required' => true))
-            ->add('on_job', 'date', array('label'=> '* On Job', 'required' => true))
+            ->add('on_job', 'date', array('label'=> '* On Job', 'required' => true,'years'       => range(date('Y'), date('Y') - 50, -1)))
             ->add('business_phone', null, array('label'=> '* Business Phone', 'required' => true))
             ->add('employer_address', null, array('label'=> '* Address', 'required' => true))
             ->add('employer_city', null, array('label'=> '* City', 'required' => true))
-            ->add('employer_state','choice',array('label'=>'* State/Prov', 'required' => true,'choices'=>array('AR','')))
+            ->add('employer_state','text',array('label'=>'* State/Prov', 'required' => true))
             ->add('employer_zip', null, array('label'=> '* Zip/Postal', 'required' => true))
 
             ->add('source', null, array('label'=> 'Source', 'required' => true))
