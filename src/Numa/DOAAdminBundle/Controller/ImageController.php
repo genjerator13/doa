da20f954ca322542057481a83e03fcba6683a2fc
root <e.medjesi@gmail.com>
2016-07-29 14:05:43 +0200

    cover photos

--- src/Numa/DOAAdminBundle/Controller/ImageController.php
+++ src/Numa/DOAAdminBundle/Controller/ImageController.php
@@ -126,19 +126,20 @@ class ImageController extends Controller implements DashboardDMSControllerInterf
             $item->setDateUpdated(new \DateTime());
             $itemField->setItem($item);
             //dump($itemField);
-            $item->setCoverPhoto($item->getCoverImageSrc());
-
-            if(empty($item->getCoverImageSrc())){
-                $item->setCoverPhoto($itemField->getFieldStringValue());
-            }
+//            $item->setCoverPhoto($item->getCoverImageSrc());
+//
+//            if(empty($item->getCoverImageSrc())){
+//                $item->setCoverPhoto($itemField->getFieldStringValue());
+//            }
 
             $itemField->setListingfield($ImageList);
             $em->persist($itemField);
             $em->flush();
 
-            if(empty($item->getCoverPhoto())){
-                $em->getRepository('NumaDOAAdminBundle:Item')->setCoverPhoto($item->getId(),$itemField->getFieldStringValue());
-            }
+//            if(empty($item->getCoverPhoto())){
+//                $em->getRepository('NumaDOAAdminBundle:Item')->setCoverPhoto($item->getId(),$itemField->getFieldStringValue());
+//            }
+            $em->getRepository('NumaDOAAdminBundle:Item')->generateCoverPhotos();
         }
         die();
     }
@@ -208,8 +209,9 @@ class ImageController extends Controller implements DashboardDMSControllerInterf
             if($order==0){
                 $if = $em->getRepository("NumaDOAAdminBundle:ItemField")->find($id);
                 $item = $if->getItem();
-                $em->getRepository("NumaDOAAdminBundle:Item")->setCoverPhoto($if->getItemId(),$if->getFieldStringValue());
-                dump($if->getFieldStringValue());
+                //$em->getRepository("NumaDOAAdminBundle:Item")->setCoverPhoto($if->getItemId(),$if->getFieldStringValue());
+                $em->getRepository('NumaDOAAdminBundle:Item')->generateCoverPhotos();
+                //dump($if->getFieldStringValue());
             }
 
         }
