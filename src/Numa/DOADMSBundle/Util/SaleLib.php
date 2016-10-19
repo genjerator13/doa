bc1c019ab7bbb8dcde5a431c14d3b5279fe9bbcc
DzVlado <dzudzar.vladimir@gmail.com>
2016-10-19 11:29:28 +0200

    fix

--- src/Numa/DOADMSBundle/Util/SaleLib.php
+++ src/Numa/DOADMSBundle/Util/SaleLib.php
@@ -41,8 +41,6 @@ class SaleLib
 //                    $sale->setItem($item);
                     $em->persist($sale);
                 }
-                $sale->setVin($item->getVIN());
-                $sale->setStockNr($item->getStockNr());
                 $sale->setSellingPrice($billing->getSalePrice());
                 $sale->setWarranty1($billing->getWarranty());
                 $sale->setAdminFees1($billing->getAdminFee());
