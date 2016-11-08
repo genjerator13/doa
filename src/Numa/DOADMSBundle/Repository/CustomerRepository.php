db203005d374085939d4230e0940c49965f9a1d0
DzVlado <dzudzar.vladimir@gmail.com>
2016-11-08 10:42:41 +0100

    customer api for admin fix

--- src/Numa/DOADMSBundle/Repository/CustomerRepository.php
+++ src/Numa/DOADMSBundle/Repository/CustomerRepository.php
@@ -44,20 +44,20 @@ class CustomerRepository extends EntityRepository {
     }
 
 
-    public function findAllNotDeleted($dealer){
+    public function findAllNotDeleted(){
         $qb = $this->getEntityManager()
             ->createQueryBuilder();
         $qb->select('cust')
             ->add('from', 'NumaDOADMSBundle:Customer cust')
-            ->andWhere("cust.status NOT LIKE 'deleted' OR cust.status IS NULL");
-        if($dealer instanceof Catalogrecords){
-            $qb->andWhere("cust.dealer_id=:dealer_id")
-               ->setParameter("dealer_id",$dealer->getId());
-        }elseif($dealer instanceof DealerGroup){
-            $qb->innerJoin('NumaDOAAdminBundle:Catalogrecords', 'd', "WITH", "d.id=cust.dealer_id");
-            $qb->andWhere("d.dealer_group_id=:dealer_group_id")
-                ->setParameter("dealer_group_id",$dealer->getId());
-        }
+            ->Where("cust.status NOT LIKE 'deleted' OR cust.status IS NULL");
+//        if($dealer instanceof Catalogrecords){
+//            $qb->andWhere("cust.dealer_id=:dealer_id")
+//               ->setParameter("dealer_id",$dealer->getId());
+//        }elseif($dealer instanceof DealerGroup){
+//            $qb->innerJoin('NumaDOAAdminBundle:Catalogrecords', 'd', "WITH", "d.id=cust.dealer_id");
+//            $qb->andWhere("d.dealer_group_id=:dealer_group_id")
+//                ->setParameter("dealer_group_id",$dealer->getId());
+//        }
         //dump($qb->getQuery()->getSQL());die();
         $res = $qb->getQuery()->getResult();
 
