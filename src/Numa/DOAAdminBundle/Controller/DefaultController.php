c68c18c5364356a52f40b095776afc614e7f28dc
root <e.medjesi@gmail.com>
2016-11-12 15:29:21 +0100

    logout and code fixes

--- src/Numa/DOAAdminBundle/Controller/DefaultController.php
+++ src/Numa/DOAAdminBundle/Controller/DefaultController.php
@@ -14,24 +14,4 @@ class DefaultController extends Controller
             $stats);
     }
 
-    public function loginAction()
-    {
-
-        $request = $this->getRequest();
-        $session = $request->getSession();
-
-        // get the login error if there is one
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        }
-
-        return $this->render('NumaDOAAdminBundle:Default:login.html.twig', array(
-            // last username entered by the user
-            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-            'error' => $error,
-        ));
-    }
 }
