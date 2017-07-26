163f93f9f508f47186a67ea26baf964b278344ed
root <e.medjesi@gmail.com>
2017-07-26 16:33:05 +0200

    qb code fixes

--- src/Numa/DOADMSBundle/Util/QuickbooksSaleLib.php
+++ src/Numa/DOADMSBundle/Util/QuickbooksSaleLib.php
@@ -35,43 +35,5 @@ class QuickbooksSaleLib
         return $this->dealer;
     }
 
-    /**
-     * @param \QuickBooks_IPP_Object_Vendor $qbVendor
-     * @param Vendor $vendor
-     * @return \QuickBooks_IPP_Object_Vendor
-     * Maps DMS vendor into QB vendors
-     */
-    public function addSupplier(\QuickBooks_IPP_Object_Vendor $qbVendor, Vendor $vendor)
-    {
 
-        $qbVendor->setCompanyName($vendor->getCompanyName());
-        $qbVendor->setDisplayName($vendor->getCompanyName());
-        $qbVendor->setGivenName($vendor->getFirstName());
-        $qbVendor->setFamilyName($vendor->getLastName());
-
-        $Email = new \QuickBooks_IPP_Object_PrimaryEmailAddr();
-        $Email->setAddress($vendor->getEmail());
-        $qbVendor->setPrimaryEmailAddr($Email);
-
-        $PrimaryPhone = new \QuickBooks_IPP_Object_PrimaryPhone();
-        $PrimaryPhone->setFreeFormNumber($vendor->getHomePhone());
-        $qbVendor->setPrimaryPhone($PrimaryPhone);
-
-        $Fax = new \QuickBooks_IPP_Object_Fax();
-        $Fax->setFreeFormNumber($vendor->getFax());
-        $qbVendor->setFax($Fax);
-
-        $Mobile = new \QuickBooks_IPP_Object_Mobile();
-        $Mobile->setFreeFormNumber($vendor->getMobilePhone());
-        $qbVendor->setMobile($Mobile);
-
-        $BillAddr = new \QuickBooks_IPP_Object_BillAddr();
-        $BillAddr->setLine1($vendor->getAddress());
-        $BillAddr->setCity($vendor->getCity());
-        $BillAddr->setCountry($vendor->getCountry());
-        $BillAddr->setPostalCode($vendor->getZip());
-        $BillAddr->setState($vendor->getState());
-        $qbVendor->setBillAddr($BillAddr);
-        return $qbVendor;
-    }
 }
