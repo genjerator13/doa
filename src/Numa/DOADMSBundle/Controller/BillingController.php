6a6c473570149713ed07a3bf9387888d7adc5b2d
root <e.medjesi@gmail.com>
2017-09-01 17:09:23 +0200

    mpdf fix alberta

--- src/Numa/DOADMSBundle/Controller/BillingController.php
+++ src/Numa/DOADMSBundle/Controller/BillingController.php
@@ -323,7 +323,8 @@ class BillingController extends Controller
 //            $html,
 //            200
 //        );
-        $mpdf = new \mPDF("","A4",0,"",5,5,10,10);
+        $mpdf = new \Mpdf\Mpdf(array("margin_left"=>5,"margin_right"=>5,"margin_top"=>5,"margin_bottom"=>5));
+        //$mpdf = new \mPDF("","A4",0,"",5,5,10,10);
 
         $mpdf->useOnlyCoreFonts = true;    // false is default
         $mpdf->SetProtection(array('print'));
