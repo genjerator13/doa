2fe03634d42cbb48c580cebd1bd714b8e48b8e94
root <e.medjesi@gmail.com>
2017-09-04 13:09:57 +0200

    test url

---
+++ src/Numa/DOAAdminBundle/Command/TestCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Numa\DOAAdminBundle\Command;
+
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+class ArchiveCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('numa:archive')
+            ->addArgument('function', InputArgument::OPTIONAL, 'Command name')
+            ->addArgument('param1', InputArgument::OPTIONAL, 'param1');
+    }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $command = $input->getArgument('function');
+        $param1 = $input->getArgument('param1');
+
+        if ($command == 'archive') {
+            $this->archive($param1);
+        }
+        elseif ($command == 'sold') {
+            $this->setSoldDate();
+        }
+    }
+
+    public function archive($period){
+        $em = $this->getContainer()->get('doctrine')->getManager();
+
+        $period = "-".$period;
+
+        $items = $em->getRepository('NumaDOAAdminBundle:Item')->findSoldForArchive($period);
+
+        foreach($items as $item){
+            $this->getContainer()->get('numa.dms.listing')->archiveItem($item);
+        }
+        $em->flush();
+        dump(count($items));
+    }
+
+    public function setSoldDate(){
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $items = $em->getRepository('NumaDOAAdminBundle:Item')->findBy(array('sold' => true, 'sold_date' => null));
+        foreach($items as $item){
+            $this->getContainer()->get('numa.dms.listing')->setSoldDateItem($item);
+        }
+        $em->flush();
+        dump(count($items));
+    }
+
+}
