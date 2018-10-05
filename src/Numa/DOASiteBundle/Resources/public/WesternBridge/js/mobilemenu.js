533a8eb0bc989f5812ec80e8a435e71a9dfcb5bf
root <e.medjesi@gmail.com>
2018-10-05 14:23:17 +0200

    fix mobile menui

--- src/Numa/DOASiteBundle/Resources/public/WesternBridge/js/mobilemenu.js
+++ src/Numa/DOASiteBundle/Resources/public/WesternBridge/js/mobilemenu.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function () {
-    jQuery(".mobile-menu .mobmenu li ul").hide();
+    //jQuery(".mobile-menu .mobmenu li ul").hide();
     /* hide sublinks */
     jQuery(".separator").click(function () {
         jQuery(this).next().slideToggle(300);
