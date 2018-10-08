60c05fada923e3ad7b4fa84b1b43db3ffd927179
root <e.medjesi@gmail.com>
2018-10-08 19:28:40 +0200

    ftpfeed class

--- src/Numa/DOAAdminBundle/Lib/FtpFeed.php
+++ src/Numa/DOAAdminBundle/Lib/FtpFeed.php
@@ -9,7 +9,90 @@
 namespace Numa\DOAAdminBundle\Lib;
 
 
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
 class FtpFeed
 {
+    use ContainerAwareTrait;
+    private $username;
+    private $password;
+    private $host;
+    private $ftpConnection;
+
+    /**
+     * @return mixed
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param mixed $host
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFtpConnection()
+    {
+        return $this->ftpConnection;
+    }
+
+    /**
+     * @param mixed $ftpConnection
+     */
+    public function setFtpConnection($ftpConnection)
+    {
+        $this->ftpConnection = $ftpConnection;
+    }
+
+    public function connectToFtp(){
+        $this->setFtpConnection(ftp_connect($this->getHost()));
+        ftp_login($this->getFtpConnection(), $this->getUsername(), $this->getPassword());
+        ftp_pasv($this->getFtpConnection(), true);
+    }
+
+    public function uploadFile($filename, $localfile){
+        $uploaded = ftp_put($this->getFtpConnection(), $filename, $localfile, FTP_ASCII));
+        return $uploaded;
+    }
+
 
 }
