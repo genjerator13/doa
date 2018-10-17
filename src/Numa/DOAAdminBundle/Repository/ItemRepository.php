496b28286d09f554d1325214db35309772473ef1
root <e.medjesi@gmail.com>
2018-10-17 14:45:06 +0200

    stats fix

--- src/Numa/DOAAdminBundle/Repository/ItemRepository.php
+++ src/Numa/DOAAdminBundle/Repository/ItemRepository.php
@@ -819,6 +819,7 @@ class ItemRepository extends EntityRepository
         if ($dealer instanceof Catalogrecords && $dealer->getDealerGroup() instanceof DealerGroup ) {
             //$sql = "select count(*) as count from item i left join catalog_records d ON d.id = i.dealer_id WHERE (i.archive_status is NULL or i.archive_status<>'" . Item::archived . "') and d.dealer_group_id=" . $dealer->getDealerGroup()->getId() . " and i.active=$active and" . $soldSQL. $suffixC;
         }
+
         $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
         $res = $stmt->execute();
         $res = $stmt->fetch();
