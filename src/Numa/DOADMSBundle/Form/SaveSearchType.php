db9c88492d13f5a5057d10d6598e63264a357a43
root <e.medjesi@gmail.com>
2018-11-13 14:13:39 +0100

    carfinder

--- src/Numa/DOADMSBundle/Form/SaveSearchType.php
+++ src/Numa/DOADMSBundle/Form/SaveSearchType.php
@@ -23,7 +23,7 @@ class SaveSearchType extends AbstractType
             ->add('yearFrom', null, array('label'=>'Year from'))
             ->add('yearTo', null, array('label'=>'Year to'))
             ->add('custName', null, array('label'=>'Full Name'))
-            ->add('period',ChoiceType::class,array('label'=>'Period', 'required'=>true,'choices'=>array(1=>'1 week',2=>'2 week',3=>'4 week',4=>'4 week',8=>'8 week',16=>'16 week',)))
+            ->add('period',ChoiceType::class,array('label'=>'Period', 'required'=>true,'choices'=>array(1=>'1 week',2=>'2 week',3=>'3 week',4=>'4 week',8=>'8 week',16=>'16 week',)))
             ->add('phone', null, array('label'=>'Phone'))
             ->add('email', null, array('label'=>'Email'))
             ->add('contact_by','choice',array('label'=>'Contact Me By *','choices'=>array('Email'=>"Email",'Phone'=>"Phone")))
