13e801d9aceead04798880f9ae6dc1fa7c9ddd71
root <e.medjesi@gmail.com>
2019-02-13 11:57:17 +0100

    carfinder serach fix

--- src/Numa/DOADMSBundle/Lib/NotificationClass.php
+++ src/Numa/DOADMSBundle/Lib/NotificationClass.php
@@ -69,7 +69,7 @@ class NotificationClass
             $message->setTo(array($dealer->getEmail(), $dealer->getEmail2()));
         }
         if (empty($errors)) {
-            //$ok = $mailer->send($message);
+            $ok = $mailer->send($message);
 
             $email->setStatus('Sent');
             $notification->setStatus(2);
