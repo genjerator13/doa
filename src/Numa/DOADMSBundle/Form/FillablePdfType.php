1e66f8082b3e0b8c9646f81684be65a5131f0738
root <e.medjesi@gmail.com>
2019-06-25 04:23:30 +0200

    dropdown

--- src/Numa/DOADMSBundle/Form/FillablePdfType.php
+++ src/Numa/DOADMSBundle/Form/FillablePdfType.php
@@ -10,86 +10,119 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 class FillablePdfType extends AbstractType
 {
     public $securityContext;
-    public function setSecurityContext($securityContext){
-        $this->securityContext=$securityContext;
+
+    public function setSecurityContext($securityContext)
+    {
+        $this->securityContext = $securityContext;
     }
-        /**
+
+    /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $states = array(
-            'XX' => 'Choose a State',
-            'AL' => 'Alabama',
-            'AK' => 'Alaska',
-            'AZ' => 'Arizona',
-            'AR' => 'Arkansas',
-            'CA' => 'California',
-            'CO' => 'Colorado',
-            'CT' => 'Connecticut',
-            'DE' => 'Delaware',
-            'DC' => 'District Of Columbia',
-            'FL' => 'Florida',
-            'GA' => 'Georgia',
-            'HI' => 'Hawaii',
-            'ID' => 'Idaho',
-            'IL' => 'Illinois',
-            'IN' => 'Indiana',
-            'IA' => 'Iowa',
-            'KS' => 'Kansas',
-            'KY' => 'Kentucky',
-            'LA' => 'Louisiana',
-            'ME' => 'Maine',
-            'MD' => 'Maryland',
-            'MA' => 'Massachusetts',
-            'MI' => 'Michigan',
-            'MN' => 'Minnesota',
-            'MS' => 'Mississippi',
-            'MO' => 'Missouri',
-            'MT' => 'Montana',
-            'NE' => 'Nebraska',
-            'NV' => 'Nevada',
-            'NH' => 'New Hampshire',
-            'NJ' => 'New Jersey',
-            'NM' => 'New Mexico',
-            'NY' => 'New York',
-            'NC' => 'North Carolina',
-            'ND' => 'North Dakota',
-            'OH' => 'Ohio',
-            'OK' => 'Oklahoma',
-            'OR' => 'Oregon',
-            'PA' => 'Pennsylvania',
-            'RI' => 'Rhode Island',
-            'SC' => 'South Carolina',
-            'SD' => 'South Dakota',
-            'TN' => 'Tennessee',
-            'TX' => 'Texas',
-            'UT' => 'Utah',
-            'VT' => 'Vermont',
-            'VA' => 'Virginia',
-            'WA' => 'Washington',
-            'WV' => 'West Virginia',
-            'WI' => 'Wisconsin',
-            'WY' => 'Wyoming',
-            'TT' => 'Trinidad and Tobago',
-        );
+            'USA' => array(
+                'XX' => 'Choose a State',
+                'AL' => 'Alabama',
+                'AK' => 'Alaska',
+                'AZ' => 'Arizona',
+                'AR' => 'Arkansas',
+                'CA' => 'California',
+                'CO' => 'Colorado',
+                'CT' => 'Connecticut',
+                'DE' => 'Delaware',
+                'DC' => 'District Of Columbia',
+                'FL' => 'Florida',
+                'GA' => 'Georgia',
+                'HI' => 'Hawaii',
+                'ID' => 'Idaho',
+                'IL' => 'Illinois',
+                'IN' => 'Indiana',
+                'IA' => 'Iowa',
+                'KS' => 'Kansas',
+                'KY' => 'Kentucky',
+                'LA' => 'Louisiana',
+                'ME' => 'Maine',
+                'MD' => 'Maryland',
+                'MA' => 'Massachusetts',
+                'MI' => 'Michigan',
+                'MN' => 'Minnesota',
+                'MS' => 'Mississippi',
+                'MO' => 'Missouri',
+                'MT' => 'Montana',
+                'NE' => 'Nebraska',
+                'NV' => 'Nevada',
+                'NH' => 'New Hampshire',
+                'NJ' => 'New Jersey',
+                'NM' => 'New Mexico',
+                'NY' => 'New York',
+                'NC' => 'North Carolina',
+                'ND' => 'North Dakota',
+                'OH' => 'Ohio',
+                'OK' => 'Oklahoma',
+                'OR' => 'Oregon',
+                'PA' => 'Pennsylvania',
+                'RI' => 'Rhode Island',
+                'SC' => 'South Carolina',
+                'SD' => 'South Dakota',
+                'TN' => 'Tennessee',
+                'TX' => 'Texas',
+                'UT' => 'Utah',
+                'VT' => 'Vermont',
+                'VA' => 'Virginia',
+                'WA' => 'Washington',
+                'WV' => 'West Virginia',
+                'WI' => 'Wisconsin',
+                'WY' => 'Wyoming',
+                'TT' => 'Trinidad and Tobago',
+            ), 'Canada' =>
+                array(
+                    "AL" => "Alberta",
+                    "BC" => "British Columbia",
+                    "BC" => "Manitoba",
+                    "NL" => "Newfoundland and Labrador",
+                    "NS" => "Nova Scotia",
+                    "ON" => "Ontario",
+                    "PE" => "Prince Edward Island",
+                    "QC" => "Quebec",
+                    "SK" => "Saskatchewan",
+                    "NT" => "Northwest Territories",
+                    "NU" => "Nunavut",
+                    "YT" => "Yukon",
 
+                    )
+        );
+///<select>
+//	<option value="AB">Alberta</option>
+//	<option value="BC">British Columbia</option>
+//	<option value="MB">Manitoba</option>
+//	<option value="NB">New Brunswick</option>
+//	<option value="NL">Newfoundland and Labrador</option>
+//	<option value="NS">Nova Scotia</option>
+//	<option value="ON">Ontario</option>
+//	<option value="PE">Prince Edward Island</option>
+//	<option value="QC">Quebec</option>
+//	<option value="SK">Saskatchewan</option>
+//	<option value="NT">Northwest Territories</option>
+//	<option value="NU">Nunavut</option>
+//	<option value="YT">Yukon</option>
+//</select>
         $builder
             //->add('dealer_id')
             ->add('name', 'text')
-            ->add('state','choice',array('choices'=>$states))
-            ;
-            //->add('','ckeditor');
-        if(!empty($this->securityContext) && $this->securityContext->isGranted('ROLE_ADMIN')){
-            $builder->add('helpdesc','ckeditor', array('label'=>'Help'));
+            ->add('state', 'choice', array('choices' => $states));
+        //->add('','ckeditor');
+        if (!empty($this->securityContext) && $this->securityContext->isGranted('ROLE_ADMIN')) {
+            $builder->add('helpdesc', 'ckeditor', array('label' => 'Help'));
         }
         //$builder->add('helpdesc','ckeditor', array('label'=>'Help'))
 //            ->add('settings', 'hidden')
 //            ->add('date_updated', 'hidden')
 //            ->add('date_created', 'hidden')
 //            ->add('status', 'hidden')
-            //->add('Dealer', 'hidden')
+        //->add('Dealer', 'hidden')
         ;
     }
 
